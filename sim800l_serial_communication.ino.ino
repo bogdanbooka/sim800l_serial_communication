a1e8c8caa62efa518bdcbcd9fb8de40d5838446f
Vallery Ostanko <v.ostanko@quality-lab.ru>
2017-10-15 18:31:58 +0300

    refactoring

--- sim800l_serial_communication.ino.ino
+++ sim800l_serial_communication.ino.ino
@@ -7,53 +7,49 @@
 #define RELAY_1_PIN 6
 #define RELAY_2_PIN 9
 
+//because here is used a negative relays
+#define RELAY_OFF HIGH
+#define RELAY_ON LOW
+
 SoftwareSerial serialSIM800(SIM800_RX_PIN, SIM800_TX_PIN);//Rx Tx
 
 void resetSIM800(){
   Serial.println("Reset SIM800");
   pinMode(SIM800_RESET_PIN, OUTPUT);
-  delay(500);
   digitalWrite(SIM800_RESET_PIN, HIGH);
   delay(500);
   digitalWrite(SIM800_RESET_PIN, LOW);
   delay(500);
   pinMode(SIM800_RESET_PIN, INPUT);
+  delay(1000);
   Serial.println("Reset SIM800 DONE");
 }
 
-void resetRelays(){
-  Serial.println("Reset Relays");
+void initRelays(){
+  Serial.println("Init Relays");
+
+  digitalWrite(RELAY_1_PIN, RELAY_OFF);
   pinMode(RELAY_1_PIN, OUTPUT);
-  delay(500);
-  digitalWrite(RELAY_1_PIN, LOW);
-  delay(500);
-  digitalWrite(RELAY_1_PIN, HIGH);
-  delay(500);
  
-  digitalWrite(RELAY_2_PIN, HIGH);
+  digitalWrite(RELAY_2_PIN, RELAY_OFF);
   pinMode(RELAY_2_PIN, OUTPUT);
-//  delay(500);
-  delay(500);
-  digitalWrite(RELAY_2_PIN, HIGH);
-  delay(500);
-  Serial.println("Reset Relays DONE");//*/
+  Serial.println("Init Relays DONE");
 }
 
 void setup() {
-  // put your setup code here, to run once:
   Serial.begin(19200);
   while(!Serial){}
   
   Serial.println("Starting SIM800 SMS Command Processor");
 
-
   serialSIM800.begin(19200);
   while(!serialSIM800){}
-  Serial.println("Setup Complete!");
 
   resetSIM800();
 
-  resetRelays();
+  initRelays();
+
+  Serial.println("Setup Complete!");
 }
 
 void loop() {
