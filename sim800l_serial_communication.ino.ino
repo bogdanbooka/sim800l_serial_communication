aa8da96a948c6d3a00e754260768c9572d90cb9f
Vallery Ostanko <v.ostanko@quality-lab.ru>
2017-11-05 19:39:49 +0300

    too much changes for one comment

--- sim800l_serial_communication.ino.ino
+++ sim800l_serial_communication.ino.ino
@@ -2,44 +2,52 @@
 #include <MillisTimer.h>
 #include <assert.h>
 
-#define MAIN_SENDER_NUMBER String("+71234567890")
-
 #define SIM800_RX_PIN 7
 #define SIM800_TX_PIN 8
-#define SIM800_RESET_PIN 13
 
 #define RELAY_1_PIN 6
 #define RELAY_2_PIN 9
-#define WRONG_PIN -1
+#define WRONG_RELAY_PIN -1
+
+#define RELAY_COUNT 2
 
 //because here is used a negative relays
 #define RELAY_OFF HIGH
 #define RELAY_ON LOW
 
-#define RELAY_COUNT 2
+typedef enum{
+  InvalidRelayIndex=-1,
+  Relay1=0,
+  Relay2,
+  RelayCount
+} RelayIndexes;
 
 class RelayController;
 
-RelayController* relays[RELAY_COUNT] = {0,0};
+RelayController* relays[(int)RelayCount] = {0,0};
 
 class RelayController {
   public:
     RelayController(int pin, int timeout)
       : m_pin(pin)
       , m_timer(MillisTimer(timeout)){
-      assert(m_pin != WRONG_PIN);
+      assert(m_pin != WRONG_RELAY_PIN);
       assert(timeout > 10);
       
       digitalWrite(m_pin, RELAY_OFF);
       pinMode(m_pin, OUTPUT);
-      Serial.print("Relay ");Serial.print(m_pin);Serial.println(" is ok");
+      int i =0;i;
+      Serial.print("Relay ");
+      Serial.print("on ");
+      Serial.print(m_pin);
+      Serial.println(" is ok");
     }
 
     void turnOnRelay(){
         if (m_timer.isRunning())
           return; 
 
-        Serial.print("Relay ");Serial.print(m_pin);Serial.println(" is ON");
+        Serial.print("Relay ");Serial.print(m_pin);Serial.print(m_timer.ID);Serial.println(" is ON");
         digitalWrite(m_pin, RELAY_ON);
 
         m_timer.expiredHandler(RelayController::turnOffRelay);
@@ -65,7 +73,7 @@ class RelayController {
       }
     }
 
-    int m_pin = WRONG_PIN;
+    int m_pin = WRONG_RELAY_PIN;
     MillisTimer m_timer;
 };
 
@@ -75,7 +83,7 @@ void initRelays() {
 }
 
 void updateRelays(){
-  for (int i = 0; i < RELAY_COUNT; ++i) {
+  for (int i = 0; i < (int)RelayCount; ++i) {
     RelayController* relay = relays[i];
     if (relay != 0)
       relay->update();
@@ -84,24 +92,33 @@ void updateRelays(){
 
 SoftwareSerial serialSIM800(SIM800_RX_PIN, SIM800_TX_PIN);//Rx Tx
 
+void waitForSim800Reply(){
+  do{
+    delay(30);
+  }while(!serialSIM800.available());
+}
+
 void receiveSIM800Answer(){
-  delay(100);
+  waitForSim800Reply();
   while (serialSIM800.available()){
     Serial.write(serialSIM800.read());
-    delay(100);
   }
   Serial.println();
 }
 
+#define SIM800_RESET_PIN 13
+
 void resetSIM800(){
-  Serial.println("Reset SIM800");
+  Serial.println("Reset SIM800 --------");
+  Serial.println("Set SIM800 OFF");
   pinMode(SIM800_RESET_PIN, OUTPUT);
   digitalWrite(SIM800_RESET_PIN, HIGH);
-  delay(500);
+  delay(2000);
   digitalWrite(SIM800_RESET_PIN, LOW);
-  delay(500);
+  delay(2000);
+  Serial.println("Set SIM800 ON");
   pinMode(SIM800_RESET_PIN, INPUT);
-  delay(1000);
+  delay(3000);
 
 //initial check
   serialSIM800.println("AT");
@@ -109,7 +126,10 @@ void resetSIM800(){
 
 //set full functionality
   serialSIM800.println("AT+CFUN=1");
-  receiveSIM800Answer();//OK
+  for (int i = 0; i < 5; ++i){
+    receiveSIM800Answer();//OK
+    delay(500);
+  }
 
 //set SMS format to ASCII
   serialSIM800.println("AT+CMGF=1");
@@ -134,38 +154,99 @@ void setup() {
   Serial.println("Setup Complete!");
 }
 
+
+void turnOnRelay(RelayIndexes relayIndex){
+  assert(relayIndex > InvalidRelayIndex);
+  assert(relayIndex < RelayCount);
+
+  relays[static_cast<const int>(relayIndex)]->turnOnRelay();
+}
+
+#define MAIN_SENDER_NUMBER "+71234567890"
+#define END_OF_MESSAGE "^*^*OK^*"
+#define NEW_NLRC "^*"
+
+String getSenderNumber(const String& message){message;}
+
+//AT+CMGL="REC READ"
+#define READ_SMS_LIST_COMMAND "AT+CMGR=1,0"
+#define REMOVE_READ_SMS "AT+CMGD=1,0"
+void readListOfNewSms(){
+  Serial.println("read List Of New Sms");
+  String lst;
+
+  serialSIM800.println(READ_SMS_LIST_COMMAND);
+  waitForSim800Reply();
+
+  while (serialSIM800.available())
+    lst+=(char)serialSIM800.read();
+
+  lst.replace("\n","*");
+  lst.replace("\r","^");
+  Serial.print(lst);
+  Serial.println("End read List Of New Sms");
+
+  if (!lst.endsWith(END_OF_MESSAGE)){
+    Serial.println("End is NOT ok");
+    return;
+  }
+  Serial.println("End is OK");
+  int pos = lst.indexOf(MAIN_SENDER_NUMBER);
+  if (pos == -1){
+    Serial.println("Unknown sender");
+    return;
+  }
+  Serial.println("Sender is OK");
+  //try separate message
+  //cut of "OK"
+  pos = lst.indexOf(END_OF_MESSAGE);
+  lst.remove(pos);
+  Serial.println(lst);
+  String message = lst.substring(lst.lastIndexOf(NEW_NLRC)+2);
+  Serial.println(message);
+
+  if (message == "1")
+      turnOnRelay(Relay1);
+  else if (message == "2")
+      turnOnRelay(Relay2);
+  //remove SMS AT+CMGD=1,3
+  serialSIM800.println(REMOVE_READ_SMS);
+  receiveSIM800Answer();
+}
+
+void onIncomingMessage(const String& atResponce){
+//sim --
+//+CMTI: "SM",2
+//-- sim
+  if (atResponce.indexOf("+CMTI: \"SM\"") > -1){
+    readListOfNewSms();
+  }
+}
+
 void loop() {
   updateRelays();
 
-  while (serialSIM800.available()){
-    Serial.write(serialSIM800.read());
-
-    //to do: do read a list of new SMS
-    //if it contains any sms from certain sender (specific number)
-    String senderNumber;
-    if (senderNumber == MAIN_SENDER_NUMBER){
-
-      //then check SMS text
-      char smsCommand;
-      switch (smsCommand) {
-    
-        //if the SMS text is '1', then switch on first relay: relays[0]->turnOnRelay();
-        case '1':
-          relays[0]->turnOnRelay();
-          break;
-    
-        //if the SMS text is '2', then switch on second relay: relays[1]->turnOnRelay();
-        case '2':
-          relays[1]->turnOnRelay();
-          break;
-      }
-    }
-    //remove all SMS
+  if (serialSIM800.available()){
+    Serial.print("SIM--");
+    String lst;
+    while (serialSIM800.available())
+      lst+=(char)serialSIM800.read();
+    lst.replace("\n","*");
+    lst.replace("\r","^");
+    Serial.print(lst);
+    Serial.println("--SIM");
+    onIncomingMessage(lst);
   }
 
   while (Serial.available()){
-    serialSIM800.write(Serial.read());
+    char sr = Serial.read();
+    if (sr == '~')
+      readListOfNewSms();
+    else{
+      serialSIM800.write(sr);
+    }
   }
+  delay(50);
 }
 
 void __assert(const char *__func, const char *__file, int __lineno, const char *__sexp) {
